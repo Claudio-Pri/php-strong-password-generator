8241df3d0c011e8b98815166c5ce0648f1a77537
Claudio-Pri <priv.claudio@gmail.com>
2024-09-30 16:32:53 +0200

    milestone 2

---
+++ functions.php
@@ -0,0 +1,19 @@
+<?php 
+$userLength = $_GET['user_length'];
+
+//funzione da stack overflow
+function generateRandomString($length) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[random_int(0, $charactersLength - 1)];
+    }
+
+    return $randomString;
+}
+// var_dump(generateRandomString($length = 30));
+// var_dump($userLength);
+
+?>
